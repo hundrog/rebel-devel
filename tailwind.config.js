58c9ae1801d3b2e51a711bf1cdf95329448653c9
Oscar Recio <reciog.oscar@hotmail.com>
2024-03-20 10:40:07 -0600

    feat: add theme switching

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,25 @@ export default {
     extend: {},
   },
   plugins: [],
-}
+};
 
 module.exports = {
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["cyberpunk"],
+    themes: [
+      "cyberpunk",
+      {
+        cyberdark: {
+          ...require("daisyui/src/theming/themes")["synthwave"],
+          "color-scheme": "dark",
+          "fontFamily":
+            "ui-monospace,SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace",
+          "--rounded-box": "0",
+          "--rounded-btn": "0",
+          "--rounded-badge": "0",
+          "--tab-radius": "0",
+        },
+      },
+    ],
   },
-}
-
+};
