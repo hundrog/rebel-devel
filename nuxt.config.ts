1a0aa7773d7cfe57c3fff4d983a7e69360ff7abd
Oscar Recio <reciog.oscar@hotmail.com>
2024-03-20 13:51:34 -0600

    feat: add supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/supabase"
+  ],
+  supabase: {
+    redirect: false,
+    redirectOptions: {
+      login: '/login',
+      callback: '/confirm',
+      include: ['/admin(/*)?'],
+      exclude: [],
+      cookieRedirect: false,
+    }
+  }
 })
