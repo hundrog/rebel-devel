da862bf112ea2cc946981f763f67de1afa5bd65f
Oscar Recio <reciog.oscar@hotmail.com>
2024-03-22 12:35:52 -0600

    feat: add pinia for state management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxtjs/supabase"
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase", "@pinia/nuxt"],
   supabase: {
     redirect: false,
     redirectOptions: {
